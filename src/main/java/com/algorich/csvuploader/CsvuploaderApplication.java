43e7f445766e6b29a1b87da1631e2896a0723067
Kirill Lassounski <lassounski@gmail.com>
2025-01-20 10:49:49 +0100

    initial commit

---
+++ src/main/java/com/algorich/csvuploader/CsvuploaderApplication.java
@@ -0,0 +1,13 @@
+package com.algorich.csvuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvuploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorich/csvuploader/CsvuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorich.csvuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvuploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
